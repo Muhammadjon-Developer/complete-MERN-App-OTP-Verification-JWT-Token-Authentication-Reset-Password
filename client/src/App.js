8867e0eeeab46a208698a5020f3f26ab870a01f8
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-06-12 17:37:32 +0300

    creating files and folders react-router-dom and a part of Username.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+/** import all components */
+import Username from "./components/Username";
+import Password from "./components/Password";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import Recovery from "./components/Recovery";
+import Reset from "./components/Reset";
+import PageNotFound from "./components/PageNotFound";
+
+// Root routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username></Username>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/password",
+    element: <Password></Password>,
+  },
+  {
+    path: "/profile",
+    element: <Profile></Profile>,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: "/reset",
+    element: <Reset></Reset>,
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
 
 const App = () => {
   return (
-    <div>React Login App</div>
-  )
-}
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
+  );
+};
 
-export default App
+export default App;
